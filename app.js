1833740e0b094e7cce87aa0d9f8116df6126e23a
vqkhanh <vqkhanh2009@gmail.com>
2022-09-14 07:12:27 +0700

    25. Exploring Event Modifiers

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const app = Vue.createApp({
     };
   },
   methods: {
+    submitForm(event) {
+      event.preventDefault();
+      alert("tets");
+    },
     setName(event, lastname) {
       this.name = event.target.value + " " + lastname;
     },
